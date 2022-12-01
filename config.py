4d894e45e88554b86ef59aae40f0f73759e388be
Kyabc <0707.teriyaki@gmail.com>
2022-12-01 12:26:36 +0000

    change: change dir

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     CHANNEL_ACCESS_TOKEN: str
     USER_AGENT: str
+    MAX_API_REQUESTS: int
 
 
 settings = Settings(_env_file=".env", _env_file_encoding="utf-8")
